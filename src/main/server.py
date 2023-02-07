d9ce8d91fcf926eba6a1866b65acb07977f186ac
carterwsmith <csmithonthetrack@gmail.com>
2023-02-07 09:57:00 +0100

    feat: specify type of github request (push vs. ping)

--- src/main/server.py
+++ src/main/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, make_response, request, jsonify
+from git import Repo
 import json
 
 app = Flask(__name__)
@@ -6,9 +7,13 @@ clone_dir = "./clone_dir"
 
 @app.route("/", methods=["GET", "POST"])
 def process_github_request():
-    if "X-GitHub-Event" in request.headers:
+    if "X-GitHub-Event" in request.headers and request.headers["X-GitHub-Event"] == "push":
         try:
             payload = request.json
             return payload
         except Exception as e:
             return "Error: {}".format(e)
+    elif request.headers["X-GitHub-Event"] == "ping":
+        return make_response("GitHub ping successful", 200)
+    else:
+        return make_response("Not a GitHub request", 404)
