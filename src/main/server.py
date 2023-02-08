cbed6702703407de4581fa063642175320c17849
David Kaméus <david.kameus@gmail.com>
2023-02-08 14:32:44 +0100

    cleanup: remove debug prints and history stuff

--- src/main/server.py
+++ src/main/server.py
@@ -6,7 +6,6 @@ from utils import parse_github_payload, check_py_syntax, change_commit_status
 
 app = Flask(__name__)
 CLONE_DIR = "./tmp/"
-HISTORY_FILE = "./ci.history"
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -26,21 +25,18 @@ def process_github_request():
                 CLONE_URL, CLONE_DIR, branch=COMMIT_BRANCH
             )
 
-            print(f"LOG: Cloned repository: {CLONE_URL}/{COMMIT_BRANCH}", flush=True)
 
             # Compile and check syntax of all .py files in the cloned directory
             SYNTAX_CHECK = check_py_syntax(F_PATH=CLONE_DIR)
 
-            print(f"LOG: Checked syntax. Success: {SYNTAX_CHECK}")
 
             # Invoke tests with subprocess
             tmp_test_path = CLONE_DIR + "src/test"
-            test_output = subprocess.run(["python3", "-m", "pytest", tmp_test_path], capture_output=True)
+            test_output = subprocess.run(["python", "-m", "pytest", tmp_test_path], capture_output=True)
             TEST_RESULT = False
             if "passed" in test_output.stdout.decode("utf-8") and "failed" not in test_output.stdout.decode("utf-8"):
                 TEST_RESULT = True
 
-            print(f"LOG: Ran tests. Output: {test_output.stdout.decode('utf-8')}", flush=True)
 
             # Remove temp directory when done
             rmtree(CLONE_DIR)
@@ -50,12 +46,10 @@ def process_github_request():
                 STATUS = "success"
             else:
                 STATUS = "failure"
-            print("LOG: Attempting to change commit status...", flush=True)
             change_commit_status(OWNER_NAME=payload_data["owner_name"],
                                  REPO_NAME=payload_data["repo_name"],
                                  SHA=payload_data["sha"],
                                  STATUS=STATUS)
-            print(f"LOG: Changed commit status to: {STATUS}", flush=True)
 
             return make_response(jsonify(payload_data), 200)
         except Exception as e:
