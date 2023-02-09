0d0ce1f7ad2c29abfffa6308490441a5e7ae8f08
carterwsmith <csmithonthetrack@gmail.com>
2023-02-09 10:24:16 +0100

    test failing test

--- src/test/test_dummy.py
+++ src/test/test_dummy.py
@@ -2,7 +2,7 @@ import pytest
 from src.main.dummy import TEST_BOOL, TEST_STRING
 
 def test_dummy_bool():
-    assert TEST_BOOL == True
+    assert TEST_BOOL == False
 
 def test_dummy_string():
     assert TEST_STRING == 'Hello World'
